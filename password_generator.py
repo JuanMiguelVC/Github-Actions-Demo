8035ba78932f6cc1720576272bd3303b8ce87cd7
Juan Miguel Vime Castillo <58298904+JuanMiguelVC@users.noreply.github.com>
2025-05-09 09:59:22 +0200

    Refactor loop variables for clarity in password generation

--- password_generator.py
+++ password_generator.py
@@ -5,8 +5,8 @@ chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*(
 number_of_passwords = int(input("How many passwords do you want to generate? "))
 password_length = int(input("How long do you want your passwords to be? "))
 print("Here are your passwords:")
-for _ in range(number_of_passwords):
+for pwd in range(number_of_passwords):
     password = ""
-    for _ in range(password_length):
+    for p in range(password_length):
         password += random.choice(chars)
     print(password)
